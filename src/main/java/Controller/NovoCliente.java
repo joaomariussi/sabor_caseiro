13335c738d956dbeed3cb079111e11cb9df64f73
joaomariussi <joaomariussi10@gmail.com>
2023-05-30 20:35:33 -0300

    :rocket: Criando a função que faz o cadastro de um novo cliente.

--- src/main/java/Controller/NovoCliente.java
+++ src/main/java/Controller/NovoCliente.java
@@ -1,31 +1,32 @@
 package Controller;
 
-import Model.Usuario;
+import Model.Cliente;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class NovoUsuario {
+public class NovoCliente {
 
     public static void main(String[] args) {
 
-        //Faz a inserção de um novo usuário
+        //Faz a inserção de um novo cliente
 
             try {
                 EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("sabor_caseiro");
                 EntityManager entityManager = entityManagerFactory.createEntityManager();
 
-                Usuario novoUsuario = new Usuario("Joao","joao@gmail.com","10203040");
+                Cliente novoCliente = new Cliente("João", "01040688098", "54999957486", "Rua tal",
+                "9966000", "Campinas do Sul", "RS", "28/07/2001", "Masculino");
 
                 entityManager.getTransaction().begin();
-                entityManager.persist(novoUsuario);
+                entityManager.persist(novoCliente);
                 entityManager.getTransaction().commit();
 
                 entityManager.close();
                 entityManagerFactory.close();
 
-                System.out.println("Usuário inserido com sucesso!");
+                System.out.println("Cliente inserido com sucesso!");
 
             } catch (Exception e) {
                 e.printStackTrace();
