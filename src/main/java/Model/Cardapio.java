544896b4e8127264fed485de73a8a7d62f1ecf1f
João Pedro Mariussi <joaomariussi10@gmail.com>
2023-06-05 17:15:48 -0300

    :rocket: Adicionando os getters e setters da classe "Cardapio"

--- src/main/java/Model/Cardapio.java
+++ src/main/java/Model/Cardapio.java
@@ -1,6 +1,7 @@
 package Model;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "cardapio")
@@ -17,7 +18,42 @@ public class Cardapio {
     @Column(nullable = false)
     private Double valor;
 
-    public Cardapio() {
+    @OneToMany
+    @JoinColumn(name = "cardapio_id")
+    private List<Prato> pratos;
 
+    public Cardapio() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public List<Prato> getPratos() {
+        return pratos;
+    }
+
+    public void setPratos(List<Prato> pratos) {
+        this.pratos = pratos;
     }
 }
