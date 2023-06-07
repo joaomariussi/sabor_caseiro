d4b0929b8f04fedb47284c097c3718dc73dbc545
João Pedro Mariussi <joaomariussi10@gmail.com>
2023-06-07 15:09:22 -0300

    :rocket: Adicionando os Remove e Get por ID e por CPF.

--- src/main/java/DAO/ClienteJpaDao.java
+++ src/main/java/DAO/ClienteJpaDao.java
@@ -1,8 +1,12 @@
 package DAO;
 
+import Model.Cliente;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
+import java.util.List;
 
 public class ClienteJpaDao {
 
@@ -28,6 +32,69 @@ public class ClienteJpaDao {
         }
         return entityManager;
     }
+
+    /**
+     *
+     * @param id |
+     * @return |
+     */
+
+    public Cliente getById(final int id) {
+        return entityManager.find(Cliente.class, id);
+    }
+
+    /**
+     *
+     * @param id |
+     * @return |
+     */
+
+    public void removeById(final int id) {
+        try {
+            Cliente cliente = getById(id);
+            entityManager.getTransaction().begin();
+            entityManager.remove(cliente);
+            entityManager.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            entityManager.getTransaction().rollback();
+        }
+    }
+
+    /**
+     * @param cpf |
+     * @return |
+     */
+    public Cliente getByCpf(final String cpf) {
+        Query query = entityManager.createQuery("SELECT c FROM Cliente c WHERE c.cpf = :cpf");
+        query.setParameter("cpf", cpf);
+        return (Cliente) query.getSingleResult();
+    }
+
+    /**
+     * @return |
+     */
+
+    @SuppressWarnings("unchecked")
+    public List<Cliente> listarTodos() {
+        return entityManager.createQuery("FROM " + Cliente.class.getName()).getResultList();
+    }
+
+    /**
+     * @param cliente |
+     */
+    public Cliente persist(Cliente cliente) {
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.merge(cliente);
+            entityManager.getTransaction().commit();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            entityManager.getTransaction().rollback();
+        }
+        return cliente;
+    }
+
 
 
 
