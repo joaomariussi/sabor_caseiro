a1456bb777cf54895d57e32706242450665b2464
joaomariussi <joaomariussi10@gmail.com>
2023-06-08 17:04:43 -0300

    :rocket: Adicionando o botão de voltar na view cadastros.fxml

--- src/main/java/Controller/CadastroController.java
+++ src/main/java/Controller/CadastroController.java
@@ -1,23 +1,39 @@
 package Controller;
 
 import Application.Main;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class CadastroController extends Main {
 
-    public void cadastroCliente() throws IOException {
+    @FXML public Button cadastrarCliente;
+    @FXML public Button voltarCadastro;
+
+    public void cadastrarCliente() throws IOException {
         Stage stage = (Stage) base.getScene().getWindow();
         Scene scene = new Scene(
-                new FXMLLoader( getClass().getResource("/view/Records/client.fxml") ).load()
+                new FXMLLoader( getClass().getResource("/view/Records/cliente.fxml") ).load()
         );
         stage.setTitle("Cadastro de Cliente");
         stage.setScene(scene);
         stage.show();
     }
 
+    /**
+     *
+     * @throws IOException |
+     */
+    @FXML public void voltarCadastro() throws IOException {
+        Stage stage = (Stage) base.getScene().getWindow();
+        Scene scene = new Scene( new FXMLLoader( getClass().getResource("/view/Application/main.fxml") ).load() );
+        stage.setTitle("Sabor Caseiro");
+        stage.setScene(scene);
+        stage.show();
+    }
 
 }
