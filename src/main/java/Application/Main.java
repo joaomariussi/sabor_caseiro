c86c3ceb405ce0259f96ecc8b5678ddd571e87da
joaomariussi <joaomariussi10@gmail.com>
2023-06-12 20:13:11 -0300

    :rocket: Adicionando o cadastro de cliente no meu arquivo main.

--- src/main/java/Application/Main.java
+++ src/main/java/Application/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
     @FXML public Pane base;
     @FXML public Button novoPedido;
     @FXML public Button devolutionAction;
-    @FXML public Button cadastro;
+    @FXML public Button cadastrarCliente;
 
     /**
      * @param args |
@@ -51,14 +51,13 @@ public class Main extends Application {
         stage.show();
     }
 
-    public void cadastro() throws IOException {
+    public void cadastrarCliente() throws IOException {
         Stage stage = (Stage) base.getScene().getWindow();
         Scene scene = new Scene(
-                new FXMLLoader( getClass().getResource("/view/Cadastros/cadastros.fxml") ).load()
+                new FXMLLoader( getClass().getResource("/view/NovoCliente/novoCliente.fxml") ).load()
         );
-        stage.setTitle("Cadastros");
+        stage.setTitle("Cadastro de Cliente");
         stage.setScene(scene);
-        stage.setResizable(false);
         stage.show();
     }
 
