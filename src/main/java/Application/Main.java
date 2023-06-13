1d2c7515dd687ed61beb164fd377bfa8c8781107
João Pedro Mariussi <joaomariussi10@gmail.com>
2023-06-13 10:54:16 -0300

    :rocket: Adicionando o cadastrarPrato no arquivo principal(main).

--- src/main/java/Application/Main.java
+++ src/main/java/Application/Main.java
@@ -16,9 +16,9 @@ public class Main extends Application {
 
     @FXML public Pane base;
     @FXML public Button novoPedido;
-    @FXML public Button devolutionAction;
     @FXML public Button cadastrarCliente;
     @FXML public Button cadastrarCardapio;
+    @FXML public Button cadastrarPrato;
 
     /**
      * @param args |
@@ -57,7 +57,7 @@ public class Main extends Application {
         Scene scene = new Scene(
                 new FXMLLoader( getClass().getResource("/view/NovoCardapio/novoCardapio.fxml") ).load()
         );
-        stage.setTitle("Cadastro de Cliente");
+        stage.setTitle("Cadastro de Cardápio");
         stage.setScene(scene);
         stage.show();
     }
@@ -72,12 +72,13 @@ public class Main extends Application {
         stage.show();
     }
 
-
-
-
-
-
-
-
-
+    public void cadastrarPrato() throws IOException {
+        Stage stage = (Stage) base.getScene().getWindow();
+        Scene scene = new Scene(
+                new FXMLLoader( getClass().getResource("/view/NovoPrato/novoPrato.fxml") ).load()
+        );
+        stage.setTitle("Cadastro de Pratos");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
