ad90db0d815f730c66bd8a803e3b4f8e08588aad
João Pedro Mariussi <joaomariussi10@gmail.com>
2023-06-13 10:56:09 -0300

    :rocket: Adicionando o botão voltar no CardapioController.

--- src/main/java/Controller/CardapioController.java
+++ src/main/java/Controller/CardapioController.java
@@ -1,4 +1,45 @@
 package Controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.io.IOException;
+
+
 public class CardapioController {
+
+
+    @FXML public Pane base;
+
+    @FXML
+    public javafx.scene.control.Button botaoVoltar;
+
+    /**
+     *
+     * @throws IOException |
+     */
+    public void botaoVoltar() throws IOException {
+        Object[] opcoes = { "Cancelar", "Confirmar" };
+        int back = JOptionPane.showOptionDialog(
+                null,
+                "Ao voltar, todos os seus dados não salvos serão perdidos!",
+                "Atenção",
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.WARNING_MESSAGE,
+                null, opcoes, opcoes[0]
+        );
+        if (back == 1) {
+            Stage stage = (Stage) base.getScene().getWindow();
+            Scene scene = new Scene(
+                    new FXMLLoader( getClass().getResource("/view/Application/main.fxml") ).load()
+            );
+            stage.setTitle("Sabor Caseiro");
+            stage.setScene(scene);
+            stage.show();
+        }
+    }
 }
