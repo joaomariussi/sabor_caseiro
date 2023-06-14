94668bc9958ec555d5fda94de3e30cfcbe245867
João Pedro Mariussi <joaomariussi10@gmail.com>
2023-06-14 17:18:03 -0300

    :rocket: Colocando o getById para a classe PratoJpaDao

--- src/main/java/DAO/PratoJpaDao.java
+++ src/main/java/DAO/PratoJpaDao.java
@@ -33,12 +33,32 @@ public class PratoJpaDao {
         return entityManager;
     }
 
+    // lista os cardápios
     public List<Cardapio> getAllCardapios() {
         try {
             entityManager.getTransaction().begin();
             List<Cardapio> cardapios = entityManager.createQuery("SELECT c FROM Cardapio c", Cardapio.class).getResultList();
             entityManager.getTransaction().commit();
             return cardapios;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            entityManager.getTransaction().rollback();
+            return null;
+        }
+    }
+
+    /**
+     *
+     * @param id |
+     * @return |
+     */
+
+    public PratosCardapio getById(final int id) {
+        try {
+            entityManager.getTransaction().begin();
+            PratosCardapio pratosCardapio = entityManager.find(PratosCardapio.class, id);
+            entityManager.getTransaction().commit();
+            return pratosCardapio;
         } catch (Exception ex) {
             ex.printStackTrace();
             entityManager.getTransaction().rollback();
