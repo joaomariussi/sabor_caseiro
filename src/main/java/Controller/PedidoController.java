a53fc60cc96c9bb66cba3fe463342f27697416f8
João Pedro Mariussi <joaomariussi10@gmail.com>
2023-06-16 09:36:53 -0300

    :rocket: Chamando a função de listar os objetos em seus ChoiceBox.

--- src/main/java/Controller/PedidoController.java
+++ src/main/java/Controller/PedidoController.java
@@ -5,19 +5,21 @@ import Model.Cardapio;
 import Model.Cliente;
 import Model.Pedido;
 import Model.StatusPedido;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 import javax.swing.*;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.List;
 
 public class PedidoController {
 
@@ -33,6 +35,8 @@ public class PedidoController {
 
     @FXML
     public javafx.scene.control.TextField valor_total;
+    @FXML
+    public javafx.scene.control.TextField observacoes;
 
     @FXML
     private ChoiceBox<Cardapio> selecioneCardapio;
@@ -53,16 +57,34 @@ public class PedidoController {
         this.dao = PedidoJpaDao.getInstance();
     }
 
-    @FXML
-    private void initialize() {
-        // Configurar as ChoiceBox com os valores adequados
-        selecioneCliente.setItems((ObservableList<Cliente>) dao.getAllClientes());
-        selecioneStatus.setItems((ObservableList<StatusPedido>) dao.getAllStatusPedidos());
-        selecioneCardapio.setItems((ObservableList<Cardapio>) dao.getAllCardapios());
+    public void initialize() {
+
+        PedidoJpaDao pedidoJpaDao = PedidoJpaDao.getInstance();
+
+        // Recupera os clientes do banco de dados
+        List<Cliente> clientes = pedidoJpaDao.getAllClientes();
+
+        // Preenche o ChoiceBox com os clientes
+        ObservableList<Cliente> clienteList = FXCollections.observableArrayList(clientes);
+        selecioneCliente.setItems(clienteList);
+
+        // Recupera os status do pedido do banco de dados
+        List<StatusPedido> statusPedidos = pedidoJpaDao.getAllStatusPedido();
+
+        // Preenche o ChoiceBox com os status do pedido
+        ObservableList<StatusPedido> statusPedidoList = FXCollections.observableArrayList(statusPedidos);
+        selecioneStatus.setItems(statusPedidoList);
+
+        // Recupera os cardápios do banco de dados
+        List<Cardapio> cardapios = pedidoJpaDao.getAllCardapio();
+
+        // Preenche o ChoiceBox com os cardápios
+        ObservableList<Cardapio> cardapioList = FXCollections.observableArrayList(cardapios);
+        selecioneCardapio.setItems(cardapioList);
     }
 
-    @FXML
-    private void botaoSalvar(ActionEvent actionEvent) throws IOException {
+
+    @FXML private void botaoSalvar(ActionEvent actionEvent) throws IOException {
         Pedido pedido = new Pedido();
 
         Cliente clienteSelecionado = selecioneCliente.getValue();
@@ -70,26 +92,39 @@ public class PedidoController {
         Cardapio cardapioSelecionado = selecioneCardapio.getValue();
         LocalDate dataEntrega = data_entrega.getValue();
         String valorTotalStr = valor_total.getText();
+        pedido.setObservacoes(String.valueOf(observacoes.getText()));
 
-        // Verificar se os campos obrigatórios estão preenchidos
+
+        // Verifica se os campos obrigatórios estão preenchidos
         if (clienteSelecionado == null || statusPedidoSelecionado == null || cardapioSelecionado == null || dataEntrega == null || valorTotalStr.isEmpty()) {
             JOptionPane.showMessageDialog(null, "Por favor, preencha todos os campos obrigatórios!", "Erro", JOptionPane.ERROR_MESSAGE);
         } else {
-            // Configurar os valores no pedido
+
+            // Configura os valores no pedido
             pedido.setCliente(clienteSelecionado);
             pedido.setStatusPedido(statusPedidoSelecionado);
             pedido.setCardapio(cardapioSelecionado);
-            pedido.setData_entrega(String.valueOf(data_entrega));
+            pedido.setObservacoes(String.valueOf(observacoes));
             pedido.setValorTotal(Double.parseDouble(valorTotalStr));
+            pedido.setData_entrega(dataEntrega);
 
-            // Salvar o pedido no banco de dados
+            // Salva o pedido no banco de dados
             dao.persist(pedido);
-
             JOptionPane.showMessageDialog(null, "Pedido cadastrado com sucesso");
+            camposLimpos();
 
         }
     }
 
+    //limpa os campos do formulário
+    private void camposLimpos() {
+        selecioneCliente.setValue(null);
+        selecioneStatus.setValue(null);
+        selecioneCardapio.setValue(null);
+        data_entrega.setValue(null);
+        valor_total.setText("");
+        observacoes.setText("");
+    }
 
     public void botaoVoltar() throws IOException {
         Object[] opcoes = { "Cancelar", "Confirmar" };
